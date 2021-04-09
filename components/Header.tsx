633faea2331da12c095fe1138c0be57a7353c82d
VITALIY1986 <staslego1986@gmail.com>
2021-04-09 12:53:03 +0300

    d

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header  className="fixed container mx-auto bg-black z-10">
-      <div className="px-6 pb-1 flex justify-between items-center ">
+      <div className="px-6 pb-5 flex justify-between items-center ">
       <button className="flex">
         <img className="mr-1" src="/static/image 2.svg" alt="" />
         <img className="mr-1" src="/static/image 3.svg" alt=""  />
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -7,13 +7,13 @@ interface IProductListProps {
 const ProductList = (props: IProductListProps) => {
   return (
     <div className="flex flex-col items-center product-list text-center">
-      <h1 className="text-white uppercase text-4xl leading-10 ">in caviar<br/>
+      <h1 className="text-white uppercase text-4xl leading-10 font">in caviar<br/>
 we trust</h1>
-      <div className="flex mt-4 mb-10 ease-linea  ">
-        <img className="mr-1.5 w-1.5" src="/static/image 2.svg" alt=""  />
-        <img className="mr-1.5 mt-2 w-1.5" src="/static/image 3.svg" alt="" />
-        <img className="w-1.5"src="/static/image 6.svg" alt=""/>
-      </div >
+<div className="flex mt-4 mb-10 ease-linea  ">
+  <img className="mr-1.5 w-1.5" src="/static/image 2.svg" alt=""  />
+  <img className="mr-1.5 mt-2 w-1.5" src="/static/image 3.svg" alt="" />
+  <img className="w-1.5"src="/static/image 6.svg" alt=""/>
+</div >
       {props.products.map((productss, index) => <Product product={productss} key={index}/>)}
     </div>
   )
